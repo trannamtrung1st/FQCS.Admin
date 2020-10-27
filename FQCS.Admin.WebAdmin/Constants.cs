c4c102cf607c22346607d2a299909e6089e095fa
trannamtrung1st <trannamtrung1st@gmail.com>
2020-10-27 11:04:25 +0700

    add index product model

--- FQCS.Admin.WebAdmin/Constants.cs
+++ FQCS.Admin.WebAdmin/Constants.cs
@@ -24,6 +24,9 @@ namespace FQCS.Admin.WebAdmin
             public const string PRODUCTION_LINE = "/productionline";
             public const string PRODUCTION_LINE_CREATE = "/productionline/create";
             public const string PRODUCTION_LINE_DETAIL = "/productionline/{id}";
+            public const string PRODUCT_MODEL = "/productmodel";
+            public const string PRODUCT_MODEL_CREATE = "/productmodel/create";
+            public const string PRODUCT_MODEL_DETAIL = "/productmodel/{id}";
             public const string ADMIN_ONLY = "/adminonly";
             public const string ACCESS_DENIED = "/accessdenied";
             public const string STATUS = "/status";
@@ -54,6 +57,7 @@ namespace FQCS.Admin.WebAdmin
             public const string RESOURCE = "resource";
             public const string DEFECT_TYPE = "defect_type";
             public const string PRODUCTION_LINE = "production_line";
+            public const string PRODUCT_MODEL = "product_model";
             public const string ADMIN_ONLY = "admin_only";
         }
     }
---
+++ FQCS.Admin.WebAdmin/Pages/ProductModel/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FQCS.Admin.WebAdmin.Models;
+using TNT.Core.Http.DI;
+
+namespace FQCS.Admin.WebAdmin.Pages.ProductModel
+{
+    [InjectionFilter]
+    public class IndexModel : BasePageModel<IndexModel>
+    {
+        public void OnGet()
+        {
+            SetPageInfo();
+        }
+
+        protected override void SetPageInfo()
+        {
+            Info = new PageInfo
+            {
+                Title = "Product model",
+                Menu = Constants.Menu.PRODUCT_MODEL,
+                BackUrl = BackUrl ?? Constants.Routing.DASHBOARD
+            };
+        }
+    }
+}
