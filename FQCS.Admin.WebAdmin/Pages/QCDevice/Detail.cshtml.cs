9303f06b56755f2c8d06dfca70e15c3b31ab28e7
trannamtrung1st <trannamtrung1st@gmail.com>
2020-10-27 14:11:20 +0700

    add detail qc device

---
+++ FQCS.Admin.WebAdmin/Pages/QCDevice/Detail.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using FQCS.Admin.WebAdmin.Models;
+using TNT.Core.Http.DI;
+
+namespace FQCS.Admin.WebAdmin.Pages.QCDevice
+{
+    [InjectionFilter]
+    public class DetailModel : BasePageModel<DetailModel>
+    {
+        public int Id { get; set; }
+        public void OnGet(int id)
+        {
+            SetPageInfo();
+            Id = id;
+        }
+
+        protected override void SetPageInfo()
+        {
+            Info = new PageInfo
+            {
+                Title = "QC device detail",
+                Menu = Constants.Menu.QC_DEVICE,
+                BackUrl = BackUrl ?? Constants.Routing.QC_DEVICE
+            };
+        }
+    }
+}
