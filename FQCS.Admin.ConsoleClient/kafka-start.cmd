a713c3ac8bf2eefafb80a649f9798b39a91400b7
trannamtrung1st <trannamtrung1st@gmail.com>
2020-10-27 23:15:06 +0700

    add password for kafka

--- FQCS.Admin.ConsoleClient/kafka-start.cmd
+++ FQCS.Admin.ConsoleClient/kafka-start.cmd
@@ -1,3 +1,4 @@
+set KAFKA_OPTS=-Djava.security.auth.login.config=N:\Workspace\Capstone\FQSC.Admin\FQCS.Admin\FQCS.Admin.ConsoleClient\kafka_server_jaas.conf
 N:
 cd N:\ITs\Kafka\kafka_2.12-2.4.1\bin\windows
 kafka-server-start.bat ../../config/server.properties
