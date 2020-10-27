a7da411ae87c9f2964198af8fd1518e2eafddda7
trannamtrung1st <trannamtrung1st@gmail.com>
2020-10-27 11:15:49 +0700

    add create pro model

---
+++ FQCS.Admin.WebAdmin/Pages/ProductModel/Create.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FQCS.Admin.WebAdmin.Models;
+using TNT.Core.Http.DI;
+
+namespace FQCS.Admin.WebAdmin.Pages.ProductModel
+{
+    [InjectionFilter]
+    public class CreateModel : BasePageModel<CreateModel>
+    {
+        public void OnGet()
+        {
+            SetPageInfo();
+        }
+
+        protected override void SetPageInfo()
+        {
+            Info = new PageInfo
+            {
+                Title = "Create product model",
+                Menu = Constants.Menu.PRODUCT_MODEL,
+                BackUrl = BackUrl ?? Constants.Routing.PRODUCT_MODEL
+            };
+        }
+    }
+}
