203204ea8ff3defce5065301772b5591b999ba22
trannamtrung1st <trannamtrung1st@gmail.com>
2020-12-03 13:18:55 +0700

    change console client

--- FQCS.Admin.ConsoleClient/kafka-start.cmd
+++
@@ -1,4 +0,0 @@
-set KAFKA_OPTS=-Djava.security.auth.login.config=N:\Workspace\Capstone\FQSC.Admin\FQCS.Admin\FQCS.Admin.ConsoleClient\kafka_server_jaas.conf
-N:
-cd N:\Servers\Kafka-Capstone\bin\windows
-kafka-server-start.bat ../../config/server.properties
